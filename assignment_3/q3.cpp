6ab7fda43a0138ff71eb9966a88a2b075c24c424
andrewmigoetz <93052767+andrewmigoetz@users.noreply.github.com>
2024-10-12 00:00:11 -0500

    Update q3.cpp

--- assignment_3/q3.cpp
+++ assignment_3/q3.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <iomanip>
 
@@ -7,31 +8,50 @@ int countOccurances(int arr[], int size, int n);
 void sortArray(int arr[], int size);
 
 int main() {
-	int size = 0;
-	    
-	int arr[50];
+    int size = 0;
 
-	// Read in the values into an array
-	cout << "Enter values ended with : ";
-	
+    int arr[50];
 
-	sortArray(arr,size);
+    // Read in the values into an array
+    cout << "Enter size of array: " << endl;
+    cin >> size;
+    cout << "Enter array elements: " << endl;
 
-	cout << left << setw(10) << "N" << setw(10) << "Count" << endl;
-	
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
 
-	return 0;
+    sortArray(arr,size);
+
+    cout << left << setw(10) << "Number" << setw(10) << "Count" << endl;
+    for (int i = 0; i < size; i++) {
+
+    }
+
+    return 0;
 }
 
-int countOccurances(int *arr, int size, int n) {
-	int count = 0;
-	
+int countOccurences(int *arr, int size, int n) {
+    int count = 0;
+    for(int i = 0; i < size; i++) {
+        if(arr[i] == n){
+            count++;
+        }
+        i++;
+    }
+    return count;
 }
 
 // I used bubble sort since that is covered in the text for this chapter
 void sortArray(int *arr, int size) {
-	int temp = 0;
-
-
+    // Bubble sort program from section 5.9 in the textbook
+    for (int i = size-1; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
 }
-
